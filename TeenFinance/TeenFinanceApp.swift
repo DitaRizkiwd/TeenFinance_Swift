075908e17f93565cf1bb4a03eb1f303f41347d17
DitaRizkiwd <rdita201@gmail.com>
2024-05-02 09:48:32 +0700

    Initial Commit

---
+++ TeenFinance/TeenFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeenFinanceApp.swift
+//  TeenFinance
+//
+//  Created by MACBOOK PRO on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TeenFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
